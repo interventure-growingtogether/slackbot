eeb5b6e961e483323d02986569970ae8d8a39b35
Ilija Ivosevic <ilija.ivosevic@tamedia.ch>
2019-12-24 14:15:56 +0100

    add event CRUD operations

--- server/api/event/event.service.js
+++ server/api/event/event.service.js
@@ -1,4 +1,30 @@
+const Event = require('./event.model');
+
+// Fetch all events
+const findAllEvents = () => Event.find({});
+
 // Add event
+const addEvent = (title, description, date) => {
+  const event = new Event({
+    title,
+    description,
+    date
+  });
+  return event.save();
+};
+
 // Change event
+const updateEvent = (id, params) => Event.findByIdAndUpdate(id, params);
+
 // Delete event
+const deleteEvent = (id) => Event.findByIdAndDelete(id);
+
 // Mark it finished
+// TODO
+
+module.exports = {
+  findAllEvents,
+  addEvent,
+  updateEvent,
+  deleteEvent
+};
