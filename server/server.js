5e98b8b710d73c7e187437ad5aacd46ca1d9494d
Lazar <lazav94@gmail.com>
2019-12-24 10:17:09 +0100

    first 🔥:

--- server/server.js
+++ server/server.js
@@ -1,18 +1,42 @@
 const express = require("express");
+const expressStaticGzip = require("express-static-gzip");
+
 require("dotenv").config();
+const next = require("next");
 
-const app = express();
-const PORT = process.env.PORT || 8080;
-// db config & connecting
-require("./services/db").connect();
+// const clientRoutes = require("./routes/clientRoutes");
+const app = next({ dev: true, dir: "./client" });
+const handle = app.getRequestHandler();
 
-// routes
-require("./routes")(app);
+const prepareApp = () => {
+  return new Promise(async (resolve, reject) => {
+    await app.prepare();
 
-// dialogflow
-require("./bot/dialogflow");
-require("./bot/slack");
+    const expressServer = express();
 
-app.listen(PORT, () => {
-  console.log(`The Hacathon is 🏃‍♂️ on port ${PORT}`);
-});
+    expressServer.use(express.json());
+    expressServer.use(express.urlencoded({ extended: true }));
+    const PORT = process.env.PORT || 8080;
+
+    expressServer.use("/_next", expressStaticGzip("./_next"));
+
+    // expressServer.use(clientRoutes(app));
+
+    require("./routes")(app);
+
+    // db config & connecting
+    require("./services/db").connect();
+
+    // routes
+
+    // dialogflow
+    require("./bot/dialogflow");
+    require("./bot/slack");
+
+    expressServer.listen(PORT, () => {
+      console.log(`The Hacathon is 🏃‍♂️ on port ${PORT}`);
+    });
+  });
+};
+
+module.exports = prepareApp();
