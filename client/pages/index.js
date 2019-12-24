c99057631c92db42cce58cf5ba7441fb5dddc54c
Dusan Stojanovic <dusan.stojanovic@carcodex.com>
2019-12-24 12:44:58 +0100

    Adding 'Add' button

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,49 @@
 import React from 'react'
-import { Button, Typography, Paper } from '@material-ui/core';
+import { Fab, Tooltip, Typography, Paper, makeStyles } from '@material-ui/core';
+import Head from 'next/head';
+import { Container } from 'next/app';
+import AddIcon from '@material-ui/icons/Add';
 
-const Home = () => (
-  <div>
-    <Paper>
-      <Typography variant="h3">
-        Svi postovi
-      </Typography>
-    </Paper>
-    <Button variant="contained" color="primary">
-      Primary
-    </Button>
-  </div>
-)
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+  },
+  addButton: {
+    position: 'fixed',
+    bottom: 30,
+    right: 30
+  }
+}));
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Head>
+        <title>IV SlackBot | Članci</title>
+      </Head>
+      <Paper>
+        <Typography variant="h3">
+          Svi postovi
+        </Typography>
+      </Paper>
+      <Container>
+        <Paper>
+
+        </Paper>
+      </Container>
+      <Tooltip title="Add">
+        <Fab
+          color="primary"
+          aria-label="add"
+          className={classes.addButton}
+        >
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </div>
+  )
+}
 
 export default Home
